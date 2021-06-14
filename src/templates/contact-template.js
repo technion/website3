eb0f8afef827c89bf94b543b6702ec2ebd9a9247
Technion <technion@lolware.net>
2021-06-14 15:50:26 +1000

    Updated contact page and headers

--- src/templates/contact-template.js
+++ src/templates/contact-template.js
@@ -1,16 +1,59 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Layout from '../components/layout';
 import { graphql } from 'gatsby';
 import styled from 'styled-components';
+
+
+const captcha = `
+<iframe id="doom_captcha" src="https://vivirenremoto.github.io/doomcaptcha/captcha.html?version=17&countdown=on&enemies=4" style="width:300px;height:150px;border:2px black solid;"></iframe>
+
+<script>
+// Minor obfuscation
+const getAddress = () => {
+  const encoded = "d!!jaG5pb25AbG9sd2FyZS5uZXQ=";
+  return atob(encoded.replace("!!", "GV"));
+
+};
+
+window.addEventListener('message', function(e) {
+    if (e.origin.indexOf('vivirenremoto.github.io') > -1) {
+        document.getElementById('doom_captcha').style.borderColor = 'black';
+        const item = document.getElementById('emailoutput');
+        item.innerHTML = "Contact address: " + getAddress();
+    }
+}, false);
+</script>
+
+`
+
+const DangerouslySetHtmlContent = (props) => {
+  //https://github.com/christo-pr/dangerously-set-html-content#readme
+  const { html, ...rest } = props
+  const divRef = useRef(null)
+
+  useEffect(() => {
+    if (!html) return
+
+    const slotHtml = document.createRange().createContextualFragment(html) // Create a 'tiny' document and parse the html string
+    divRef.current.innerHTML = '' // Clear the container
+    divRef.current.appendChild(slotHtml) // Append the new content
+  }, [html])
+
+
+  return (
+    <div {...rest} ref={divRef}></div>
+  )
+}
+
 
 const ContactTemplate = ({ data }) => {
   const { html, frontmatter } = data.markdownRemark;
 
   return (
     <Layout title={frontmatter.title}>
-    <div>Unfinished Title
     <ContactCopy dangerouslySetInnerHTML={{ __html: html }} />
-    </div>
+    <DangerouslySetHtmlContent html={captcha} />
+    <div id="emailoutput"></div>
     </Layout>
   );
 };
