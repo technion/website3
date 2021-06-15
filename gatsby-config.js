4c7b689f0470312ee204ccb4aad7aea035389e49
Technion <technion@lolware.net>
2021-06-15 19:46:51 +1000

    Google analytics. HSTS

--- gatsby-config.js
+++ gatsby-config.js
@@ -141,6 +141,20 @@ module.exports = {
       options: {
         siteUrl: `https://lolware.net`,
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-ZHX6RWFWML", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+        },
+      },
+    },
   ],
 };
